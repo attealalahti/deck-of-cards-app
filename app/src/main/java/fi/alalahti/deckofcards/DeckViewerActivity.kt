9c508aeac13d320c60811d6b01a928209824ee88
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-17 22:53:33 +0300

    Modify DeckViewerActivity: display deck_id.

--- app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
+++ app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
@@ -1,11 +1,14 @@
 package fi.alalahti.deckofcards
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DeckViewerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deck_viewer)
+        val deck_id = intent.getStringExtra("deck_id")
+        findViewById<TextView>(R.id.textView).text = deck_id
     }
 }
