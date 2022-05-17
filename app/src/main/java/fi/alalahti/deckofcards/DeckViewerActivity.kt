78067366ad5d19eec2c4231c0c1d01ede00a577d
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-17 20:22:32 +0300

    Add deck viewer activity.

---
+++ app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
@@ -0,0 +1,11 @@
+package fi.alalahti.deckofcards
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DeckViewerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deck_viewer)
+    }
+}
