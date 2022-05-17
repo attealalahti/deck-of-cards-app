12619433f8c0ab5b7e58257c80b91ffb66fc34a6
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-17 20:22:54 +0300

    Add button to go from main activity to deck viewer.

--- app/src/main/java/fi/alalahti/deckofcards/MainActivity.kt
+++ app/src/main/java/fi/alalahti/deckofcards/MainActivity.kt
@@ -1,11 +1,18 @@
 package fi.alalahti.deckofcards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createDeck(button : View) {
+        val intent = Intent(this, DeckViewerActivity::class.java)
+        startActivity(intent)
     }
 }
