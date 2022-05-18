3e30c8c0f3a0dc45f23d131648e87943d569e5f8
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-18 13:01:36 +0300

    Add button to DeckViewerActivity to draw a card.

--- app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
+++ app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
@@ -1,6 +1,8 @@
 package fi.alalahti.deckofcards
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,4 +13,11 @@ class DeckViewerActivity : AppCompatActivity() {
         val deck_id = intent.getStringExtra("deck_id")
         findViewById<TextView>(R.id.textView).text = deck_id
     }
+
+    fun drawCard(button: View) {
+        API.drawCard(intent.getStringExtra("deck_id")!!) {
+            Log.d("api", it.toString())
+        }
+    }
+
 }
