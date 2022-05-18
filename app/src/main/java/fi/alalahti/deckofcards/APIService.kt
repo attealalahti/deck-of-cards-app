a103512323350a0613b23eb63f25bbd66439a29c
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-18 12:57:47 +0300

    Modify APIService: add function to draw a card.

--- app/src/main/java/fi/alalahti/deckofcards/APIService.kt
+++ app/src/main/java/fi/alalahti/deckofcards/APIService.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 val BASE_URL = "https://deckofcardsapi.com/api/"
 
@@ -13,10 +14,25 @@ data class Deck(
     val shuffled: Boolean,
     val remaining: Number
 )
+data class Draw(
+    val success: Boolean,
+    val cards: List<Card>,
+    val deck_id: String,
+    val remaining: Number
+)
+data class Card(
+    val image: String,
+    val value: String,
+    val suit: String,
+    val code: String
+)
 
 interface APIService {
     @GET("deck/new/shuffle/?deck_count=1")
     fun createDeck(): Call<Deck>
+
+    @GET("deck/{deck_id}/draw/?count=1")
+    fun drawCard(@Path("deck_id") deck_id: String): Call<Draw>
 
     companion object {
         var apiService: APIService? = null
