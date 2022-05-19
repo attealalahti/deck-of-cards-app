b67eabd352077a7a3f3d1de564a2fe30e679d358
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-19 23:05:35 +0300

    Modify DeckViewerActivity: add functionality to the shuffle button.

--- app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
+++ app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
@@ -5,8 +5,10 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 import kotlin.concurrent.thread
@@ -15,24 +17,29 @@ class DeckViewerActivity : AppCompatActivity() {
 
     lateinit var cardImageView: ImageView
     lateinit var remainingCounter: TextView
+    lateinit var shuffleButton: Button
+    lateinit var drawButton: Button
+    var deck_id: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deck_viewer)
         cardImageView = findViewById(R.id.cardImageView)
         remainingCounter = findViewById(R.id.remainingCounter)
+        shuffleButton = findViewById(R.id.shuffleButton)
+        drawButton = findViewById(R.id.drawButton)
+        deck_id = intent.getStringExtra("deck_id")
 
         // Initialize how many cards remain in the deck
-        var remaining = intent.getIntExtra("remaining", -1)
+        val remaining = intent.getIntExtra("remaining", -1)
         if (remaining != -1) {
             updateRemainingCounter(remaining)
         }
     }
 
     fun drawCard(button: View) {
-        val deck_id = intent.getStringExtra("deck_id")
         if (deck_id != null) {
-            APIService.getInstance().drawCard(deck_id).enqueue(APICallback {
+            APIService.getInstance().drawCard(deck_id!!).enqueue(APICallback {
                 Log.d("api", it.toString())
 
                 // Update imageView with the drawn card's image
@@ -51,10 +58,20 @@ class DeckViewerActivity : AppCompatActivity() {
                 // Update counter with how many cards remain in the deck
                 updateRemainingCounter(it.remaining)
 
-                // Disable after all cards have been drawn
+                // Disable shuffle and draw after all cards have been drawn
                 if (it.remaining.toInt() <= 0) {
-                    button.isEnabled = false
+                    drawButton.isEnabled = false
+                    shuffleButton.isEnabled = false
                 }
+            })
+        }
+    }
+
+    fun shuffle(button: View) {
+        if (deck_id != null) {
+            APIService.getInstance().shuffleDeck(deck_id!!).enqueue(APICallback {
+                // Show a little pop-up when shuffling is complete
+                Toast.makeText(applicationContext, "Shuffled!", Toast.LENGTH_SHORT).show()
             })
         }
     }
