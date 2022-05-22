069abe2855dadd5c999d0798eb0e487002e03c4a
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-22 10:48:27 +0300

    Modify ErrorMessenger: add a comment.

--- app/src/main/java/fi/alalahti/deckofcards/ErrorMessenger.kt
+++ app/src/main/java/fi/alalahti/deckofcards/ErrorMessenger.kt
@@ -3,6 +3,7 @@ package fi.alalahti.deckofcards
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
+// A utility class to display an error message in a specified View.
 class ErrorMessenger {
     companion object {
         fun showErrorMessage(layout: View) {
