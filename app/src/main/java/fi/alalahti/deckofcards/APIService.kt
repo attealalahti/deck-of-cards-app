a44ae20d15de08844ab1d337d70087a85c6df9fd
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-22 10:22:39 +0300

    Modify APIService: add some comments.

--- app/src/main/java/fi/alalahti/deckofcards/APIService.kt
+++ app/src/main/java/fi/alalahti/deckofcards/APIService.kt
@@ -9,12 +9,14 @@ import retrofit2.http.QueryMap
 
 val BASE_URL = "https://deckofcardsapi.com/api/"
 
+// Object returned by the API after creating or manipulating a deck
 data class Deck(
     val success: Boolean,
     val deck_id: String,
     val shuffled: Boolean,
     val remaining: Number
 )
+// Object returned by the API after drawing a card
 data class Draw(
     val success: Boolean,
     val cards: List<Card>,
@@ -28,18 +30,30 @@ data class Card(
     val code: String
 )
 
+// Interface for making API calls using Retrofit
 interface APIService {
+    // Asks the API to create a new deck.
+    // The settings map holds query parameters for the call,
+    // telling it which cards should be in the created deck.
+    // Returns information about the created deck.
     @GET("deck/new")
     fun createDeck(@QueryMap settings: Map<String, String>): Call<Deck>
 
+    // Draw a card from a specified deck.
+    // Returns information about the draw event,
+    // including which card was drawn.
     @GET("deck/{deck_id}/draw/?count=1")
     fun drawCard(@Path("deck_id") deck_id: String): Call<Draw>
 
+    // Shuffle the rest of the cards in a specified deck.
+    // Returns information about the shuffled deck.
     @GET("deck/{deck_id}/shuffle/?remaining=true")
     fun shuffleDeck(@Path("deck_id") deck_id: String): Call<Deck>
 
     companion object {
         var apiService: APIService? = null
+        // Get an instance of this interface.
+        // If an instance doesn't exist already, build one with Retrofit
         fun getInstance(): APIService {
             if (apiService == null) {
                 apiService = Retrofit.Builder()
