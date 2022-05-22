0bfd350ea08faf4a1e50243f2c964e13f559203c
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-22 10:45:40 +0300

    Modify APICallback: use try catch to detect errors and add a comment.

--- app/src/main/java/fi/alalahti/deckofcards/APICallback.kt
+++ app/src/main/java/fi/alalahti/deckofcards/APICallback.kt
@@ -4,12 +4,17 @@ import android.view.View
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
+// Implementation of Retrofit's Callback interface for easier use.
+// If the response was successful, call the callback with the body of the response.
+// Otherwise, display an error message.
+// Layout View object is needed for the error message.
 class APICallback<T>(val layout: View, val callback: (T) -> Unit) : Callback<T> {
     override fun onResponse(call: Call<T>, response: Response<T>) {
-        if (response?.body() != null) {
+        try {
             callback(response.body() as T)
-        } else {
+        } catch (e: Exception) {
             ErrorMessenger.showErrorMessage(layout)
         }
     }
