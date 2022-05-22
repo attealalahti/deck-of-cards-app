73c29323a759514e89e7779290f4b4079150888e
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-05-22 11:47:39 +0300

    Modify DeckViewerActivity: add comments.

--- app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
+++ app/src/main/java/fi/alalahti/deckofcards/DeckViewerActivity.kt
@@ -20,6 +20,12 @@ import java.io.ByteArrayOutputStream
 import java.lang.Exception
 import kotlin.concurrent.thread
 
+// Activity for drawing cards from a deck.
+// Displays drawn cards as an image that can be rotated with
+// the device's rotation sensors.
+// Also display's the card's name as text
+// and display's how many cards are left in the deck.
+// Has a button for shuffling the deck.
 class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
 
     lateinit var layout: View
@@ -48,6 +54,7 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         pseudo3DSwitch = findViewById(R.id.pseudo3DSwitch)
         deck_id = intent.getStringExtra("deck_id")
 
+        // Initialize rotation sensor
         sensorManager  = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
 
@@ -57,6 +64,9 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
             updateRemainingCounter(remainingCards)
         }
 
+        // Register rotation sensor when the 3D switch is checked
+        // and unregister it when it is unchecked.
+        // Unchecking also removes any rotation done to the image.
         pseudo3DSwitch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 registerRotationSensor()
@@ -75,6 +85,7 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this, rotationSensor)
     }
 
+    // Don't listen to the rotation sensor when paused
     override fun onPause() {
         super.onPause()
         if (pseudo3DSwitch.isChecked) {
@@ -89,6 +100,11 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    // Triggered by the Draw a Card button.
+    // Sends a request to the API to draw a card.
+    // Updates the image of the card and
+    // how many cards remain in the deck
+    // with information from the API.
     fun drawCard(button: View) {
         if (deck_id != null) {
             APIService.getInstance().drawCard(deck_id!!).enqueue(APICallback(layout) {
@@ -121,6 +137,9 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    // Triggered by the Shuffle button.
+    // Sends a request to the API to shuffle the deck.
+    // On a success shows a popup informing the user.
     fun shuffle(button: View) {
         if (deck_id != null) {
             APIService.getInstance().shuffleDeck(deck_id!!).enqueue(APICallback(layout) {
@@ -134,19 +153,23 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    // Updates the Text View displaying how many cards remain in the deck.
+    // If no cards remain, disables Draw and Shuffle buttons.
     fun updateRemainingCounter(count: Number) {
-        runOnUiThread {
-            remainingCounter.text = "${getString(R.string.cards_remaining)} $count"
-            // Disable shuffle and draw after all cards have been drawn
-            if (count.toInt() <= 0) {
-                drawButton.isEnabled = false
-                shuffleButton.isEnabled = false
-            }
+        remainingCounter.text = "${getString(R.string.cards_remaining)} $count"
+        // Disable shuffle and draw after all cards have been drawn
+        if (count.toInt() <= 0) {
+            drawButton.isEnabled = false
+            shuffleButton.isEnabled = false
         }
     }
 
+    // Rotate the card image based on the rotation sensor.
+    // The image rotates the opposite way the device is tilted.
     override fun onSensorChanged(event: SensorEvent?) {
         if (event?.sensor?.type == Sensor.TYPE_ROTATION_VECTOR) {
+            // Rotate different ways based on device orientation,
+            // so it makes more sense.
             when(resources.configuration.orientation) {
                 Configuration.ORIENTATION_LANDSCAPE -> {
                     cardImageView.rotationX = event.values[1] * -90
@@ -160,8 +183,11 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    // There is no use in this app for checking when sensor accuracy changes.
     override fun onAccuracyChanged(sensor: Sensor?, p1: Int) {}
 
+    // Generates the name of a card using its value and suit.
+    // Jokers just return "Joker".
     fun getCardName(value: String, suit: String): String {
         if (value == "JOKER") {
             return "Joker"
@@ -171,6 +197,9 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    // Saves the current card's image, its name and how many cards remain in the deck
+    // so they can be restored if the activity is destroyed,
+    // like when device orientation changes.
     override fun onSaveInstanceState(outState: Bundle) {
         if (cardImage != null) {
             // Compress image bitmap to a bytearray so it can be put into the bundle
@@ -186,6 +215,7 @@ class DeckViewerActivity : AppCompatActivity(), SensorEventListener {
         super.onSaveInstanceState(outState)
     }
 
+    // Restores the current card's image, name and how many cards remain in the deck.
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val cardImageByteArray = savedInstanceState.getByteArray("cardImage")
